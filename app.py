197ce16adff7e03c07f14f9cfe6aa701fc97fbb4
𝕿𝖍𝖊𝖆 𝕷𝖔𝖛𝖊𝖑𝖆𝖈𝖊 <thea_lvlc@Theas-MacBook-Air.local>
2026-01-13 18:13:06 -0600

    second_commit

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
+number_of_trails = st.slider('¡Número de intentos?',1,1000,10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trails} intentos en curso.')
+
 st.write('Esta app aún no es funcional. En construcción...')
