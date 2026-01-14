1c5c819ae1aa16b6d6e6b62085b8ba1b3aa58c50
𝕿𝖍𝖊𝖆 𝕷𝖔𝖛𝖊𝖑𝖆𝖈𝖊 <thea_lvlc@Theas-MacBook-Air.local>
2026-01-13 18:54:28 -0600

    graph and attempt counter adde

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
+import pandas as pd
+import scipy
 import streamlit as st
+import time
+
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no','iteraciones','media'])
 
 st.header('Lanzar una moneda')
 
-number_of_trails = st.slider('¡Número de intentos?',1,1000,10)
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+    
+    return mean
+
+number_of_trails = st.slider('¿Número de intentos?',1,1000,10)
 start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trails} intentos en curso.')
+    st.session_state['experiment_no'] += 1
+    mean = toss_coin(number_of_trails)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trails,
+                            mean]],
+                    columns=['no','iteraciones','media'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results']=st.session_state['df_experiment_results'].reset_index(drop=True)
 
-st.write('Esta app aún no es funcional. En construcción...')
+st.write(st.session_state['df_experiment_results'])
